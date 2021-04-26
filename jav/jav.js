5982c2bedf4d6d0808221e8711b03ed724c75d13
motaser <araidahmontaser@gmail.com>
2021-04-26 21:36:19 +0300

    creat about me

---
+++ jav/jav.js
@@ -0,0 +1,169 @@
+'use strict';
+let user =' to My site';
+let name = prompt ('Hello... What is Your name  ?');
+alert ('Welcome '+ name+ user);
+alert (name+'.. you will be asked five Qusetions, Answer by Yes or No PLZ.')
+let ask1 = prompt('The current is mesuerd by ambir  ?  y/n or yes/no').toLowerCase();
+
+
+switch (ask1) {
+    case 'yes' :
+      alert("your answer is correct" );
+      console.log('correct ');
+ break;
+
+ case 'y' :
+    alert("your answer is correct" );
+    console.log('correct ');
+break;
+
+    case 'no' :
+        alert("your answer is not correct" );
+        console.log('Not correct ');
+  
+     break;
+
+     case 'n' :
+        alert("your answer is not correct");
+        console.log('Not correct ');
+        break;
+
+
+        default:
+            alert("plz answer by yes or no ");
+            break;
+
+}
+
+let ask2 = prompt('The Voltage is equal to (Current * Power)  ?  y/n or yes/no').toLowerCase();
+switch (ask2) {
+    case 'no' :
+      alert("your answer is correct" );
+      console.log('correct ');
+ break;
+
+ case 'n' :
+    alert("your answer is correct" );
+    console.log('correct ');
+break;
+
+    case 'yes' :
+        alert("your answer is not correct" );
+        console.log('Not correct ');
+  
+     break;
+
+     case 'y' :
+        alert("your answer is not correct");
+        console.log('Not correct ');
+        break;
+
+
+        default:
+            alert("plz answer by yes or no ");
+            break;
+            
+
+
+
+}
+
+let ask3 = prompt('Can we use the sun power in producing the Energy  ?  y/n or yes/no').toLowerCase();
+switch (ask3) {
+    case 'yes' :
+      alert("your answer is correct" );
+      console.log('correct ');
+ break;
+
+ case 'y' :
+    alert("your answer is correct" );
+    console.log('correct ');
+break;
+
+    case 'no' :
+        alert("your answer is not correct" );
+        console.log('Not correct ');
+  
+     break;
+
+     case 'n' :
+        alert("your answer is not correct");
+        console.log('Not correct ');
+        break;
+
+
+        default:
+            alert("plz answer by yes or no ");
+            break;
+            
+
+
+
+}
+
+let ask4 = prompt(' are the electcity cables made from Iron   ?  y/n or yes/no').toLowerCase();
+switch (ask4) {
+    case 'no' :
+      alert("your answer is correct" );
+      console.log('correct ');
+ break;
+
+ case 'n' :
+    alert("your answer is correct" );
+    console.log('correct ');
+break;
+
+    case 'yes' :
+        alert("your answer is not correct" );
+        console.log('Not correct ');
+  
+     break;
+
+     case 'y' :
+        alert("your answer is not correct");
+        console.log('Not correct ');
+        break;
+
+
+        default:
+            alert("plz answer by yes or no ");
+            break;
+            
+
+
+
+}
+let ask5 = prompt('The Power is mesuerd by Volt ?  y/n or yes/no').toLowerCase();
+switch (ask5) {
+    case 'no' :
+      alert("your answer is correct" );
+      console.log('correct ');
+ break;
+
+ case 'n' :
+    alert("your answer is correct" );
+    console.log('correct ');
+break;
+
+    case 'yes' :
+        alert("your answer is not correct" );
+        console.log('Not correct ');
+  
+     break;
+
+     case 'y' :
+        alert("your answer is not correct");
+        console.log('Not correct ');
+        break;
+
+
+        default:
+            alert("plz answer by yes or no ");
+            break;
+            
+
+
+
+}
+
+
